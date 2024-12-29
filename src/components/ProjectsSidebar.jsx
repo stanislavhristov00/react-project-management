7d3b8f3637884f4816dc94c0e8821b997e49d3c4
hstanislav <stanislavhristov00@ab.vbg>
2024-12-29 18:41:22 +0200

    First commit

---
+++ src/components/ProjectsSidebar.jsx
@@ -0,0 +1,18 @@
+import Button from "./Button";
+
+export default function ProjectsSidebar() {
+  return (
+    <aside className="w-1/3 px-8 py-16 bg-stone-900 text-stone-50 md:w-72 rounded-r-xl">
+      <h2 className="mb-8 font-bold uppercase md:text-xl text-stone-200">
+        Your projects
+      </h2>
+      <div>
+        <Button>Create a new project</Button>
+      </div>
+      <ul>
+        <li>Project 1</li>
+        <li>Project 2</li>
+      </ul>
+    </aside>
+  );
+}
